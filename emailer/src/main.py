021e8859d45578065cc1d70d830fcc89128facfa
Zack Botkin <zackbotkin@gmail.com>
2024-06-05 11:16:10 -0500

    allow emaiing files from the command line

--- emailer/src/main.py
+++ emailer/src/main.py
@@ -9,6 +9,8 @@ def main():
     parser.add_argument('--text', '-t', help='the text to send')
     parser.add_argument('--recipient', '-r', help='the address to send to')
     parser.add_argument('--creds', '-c', help='the creds file')
+    parser.add_argument('--file', '-f', help='the file to send')
+    parser.add_argument('--subject', '-s', help='the email subject')
     args = parser.parse_args()
 
     if args.creds:
@@ -17,7 +19,12 @@ def main():
         creds = EmailerCreds(DEFAULT_CREDS_FILE)
 
     emailer = Emailer(creds)
-    emailer.send_text(args.recipient, args.text)
+
+    if args.file:
+        emailer.send_file(args.recipient, args.recipient, args.file, subject=args.subject)
+    else:
+        ## TODO : refactor this a little bit
+        emailer.send_text(args.recipient, args.text)
 
 if __name__ == '__main__':
     main()
